8e130d3935aa389b17e3413e184d198cded538c4
Kael Spencer <kaelspencer@gmail.com>
2012-06-25 20:33:48 -0700

    Bound the Horizontal Scrolling This change adds bounds to how far the horizontal scrolling on the timeline can go. When the scroll event 'stop' is raised, if the end point is past either end, the timeline is moved to the proper place.

--- js/timeline.js
+++ js/timeline.js
@@ -1,5 +1,32 @@
+var Timeline = {};
+
+Timeline.horizontal_scroll_stop = function(event, ui) {
+    var line = $('#line-draggable');
+    var right = line.width();
+    var visible = right + ui.position.left;
+
+    if(ui.position.left > 0) {
+        line.animate({
+            left: '-=' + ui.position.left
+        }, 500, Timeline.draw_bubbles);
+    } else if(visible < $(window).width()) {
+        move_right = $(window).width() - visible;
+
+        line.animate({
+            left: '+=' + move_right
+        }, 500, Timeline.draw_bubbles);
+    } else {
+        Timeline.draw_bubbles();
+    }
+};
+
+Timeline.draw_bubbles = function() {
+    console.log("draw bubbles");
+};
+
 $(document).ready(function() {
-    $('#first').draggable({
-        axis: 'x'
+    $('#line-draggable').draggable({
+        axis: 'x',
+        stop: Timeline.horizontal_scroll_stop
     });
 });
