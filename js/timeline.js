09884da4537a5b989f20b603a0c3b628175a5b82
Kael Spencer <kaelspencer@gmail.com>
2012-06-25 20:50:06 -0700

    Refactor Timeline.horizontal_scroll_stop There was a bug when the width of the timeline was less than the width of the viewable area. I refactored the code in this method while adding support for the above scenario.

--- js/timeline.js
+++ js/timeline.js
@@ -2,18 +2,24 @@ var Timeline = {};
 
 Timeline.horizontal_scroll_stop = function(event, ui) {
     var line = $('#line-draggable');
-    var right = line.width();
-    var visible = right + ui.position.left;
+    var width = line.width();
+    var window_width = $(window).width();
+    var visible = width + ui.position.left;
+    var move = 0;
 
-    if(ui.position.left > 0) {
-        line.animate({
-            left: '-=' + ui.position.left
-        }, 500, Timeline.draw_bubbles);
-    } else if(visible < $(window).width()) {
-        move_right = $(window).width() - visible;
+    console.log('width: ' + width);
+    console.log('visible: ' + visible);
 
+    if(ui.position.left > 0 || width < window_width) {
+        // The left side is to the right of the left edge of the screen.
+        move = -1 * ui.position.left;
+    } else if(visible < window_width) {
+        move = $(window).width() - visible;
+    }
+
+    if(move) {
         line.animate({
-            left: '+=' + move_right
+            left: '+=' + move
         }, 500, Timeline.draw_bubbles);
     } else {
         Timeline.draw_bubbles();
@@ -21,7 +27,7 @@ Timeline.horizontal_scroll_stop = function(event, ui) {
 };
 
 Timeline.draw_bubbles = function() {
-    console.log("draw bubbles");
+
 };
 
 $(document).ready(function() {
