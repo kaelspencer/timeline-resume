ac26aa4b56c56b61165c6f41482c8f650d5448f5
Kael Spencer <kaelspencer@gmail.com>
2012-06-27 18:41:38 -0700

    Migration Towards Making the Timeline an Object This change starts making the timeline a proper object. It isn't quite working as I had hoped. I think I'll take a different route eventually.
    There are also changes included here that switch the years from being in a
    span to being in a div. This allows for a fixed with. The right margin on snap
    doesn't line up properly. There needs to be a little more padding.

--- js/timeline.js
+++ js/timeline.js
@@ -1,38 +1,75 @@
-var Timeline = {};
+function timeline_init(element, years) {
+    this.container = $('#' + element);
+    this.line = $('<div class="line"/>');
+    this.draggable = $('<div id="line-draggable">');
 
-Timeline.horizontal_scroll_stop = function(event, ui) {
+    this.container.addClass('timeline');
+    this.container.append(this.line);
+    this.line.append(this.draggable);
+
+    this.draggable.draggable({
+        axis: 'x',
+        stop: Timeline.horizontal_scroll_stop
+    });
+
+    years.forEach(function(element, index, array) {
+        var el = $('<div>' + element + '</div>');
+        this.draggable.append(el);
+    }, this);
+
+    // The draggable line has a width calculated by:
+    // element_size = width of text + margin left + margin right = 100 + 20 + 80
+    // Subtract the right marging from the last div.
+    // width = (number of years) * (100 + 20 + 80) - 60
+    this.width = years.length * 200 - 60;
+    console.log(this.width);
+    console.log(this.line.width());
+
+    $('#line-draggable div').last().addClass('last');
+    this.line.css('width', '100%');
+}
+
+function timeline_horizontal_scroll_stop(event, ui) {
     var line = $('#line-draggable');
     var width = line.width();
     var window_width = $(window).width();
     var visible = width + ui.position.left;
     var move = 0;
 
-    console.log('width: ' + width);
-    console.log('visible: ' + visible);
-
     if(ui.position.left > 0 || width < window_width) {
         // The left side is to the right of the left edge of the screen.
         move = -1 * ui.position.left;
     } else if(visible < window_width) {
+        // The right side is to the left of the right edge of the screen.
+        //move = $(window).width() - visible + 120;
         move = $(window).width() - visible;
     }
 
     if(move) {
         line.animate({
             left: '+=' + move
-        }, 500, Timeline.draw_bubbles);
+        }, 500, Timeline.draw);
     } else {
-        Timeline.draw_bubbles();
+        Timeline.draw();
     }
-};
+}
 
-Timeline.draw_bubbles = function() {
+function timeline_draw() {
+    console.log('draw');
+}
 
-};
+function timeline() {
+    this.init = timeline_init;
+    this.horizontal_scroll_stop = timeline_horizontal_scroll_stop;
+    this.draw = timeline_draw;
+
+    this.line = Object;
+    this.width = 0;
+}
+
+var Timeline = new timeline();
 
 $(document).ready(function() {
-    $('#line-draggable').draggable({
-        axis: 'x',
-        stop: Timeline.horizontal_scroll_stop
-    });
+    var years = ["2003","2004","2005","2006","2007","2008","2009","2010","2011","2012"];
+    Timeline.init('timeline', years);
 });
